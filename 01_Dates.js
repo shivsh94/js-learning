34bf51d9b319c2fcee19d1d3654586e8bf55ce94
Shivam Sharma <shivamsharma23jan@gmail.com>
2024-04-10 00:26:23 +0530

    ads

---
+++ 01_Dates.js
@@ -0,0 +1,13 @@
+let myDate = new Date()
+console.log(myDate.toString());
+
+console.log(myDate.toDateString());
+
+console.log(myDate.toLocaleString());
+
+
+let myCreateDate = new Date(2024, 1, 23 )
+console.log(myCreateDate.toLocaleDateString());
+
+let myCreateDate1 = new Date(2024, 2, 24)
+console.log(myCreateDate1.toLocaleDateString());
---
+++ 01_nums_and_maths.js
@@ -0,0 +1,9 @@
+const score = 400 //Output is 400
+console.log(score);
+
+const balance = new Number(100) // Output is [Number: 100]
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
